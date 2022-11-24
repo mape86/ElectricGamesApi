5de63fcd8eddb803f5e9896b0a3ec2ee5690d331
Sondre Stokkan Spæren <89384544+Sondre-S@users.noreply.github.com>
2022-11-24 10:52:00 +0100

    Edited gameConsoleController

--- Controllers/GameConsoleController.cs
+++ Controllers/GameConsoleController.cs
@@ -24,6 +24,36 @@ public class GameConsoleController : ControllerBase
         return gameConsoles;
     }
 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<GameConsole>> GetCharacterById(int id)
+    {
+        GameConsole? chosenConsole = await context.GameConsole.FindAsync(id);
+        if (chosenConsole != null)
+        {
+            return Ok(chosenConsole);
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpGet]
+    [Route("[action]/{brand}")]
+    public async Task<ActionResult<List<Character>>> GetConsoleByBrand(string brand)
+    {
+
+        List<GameConsole> consoles = await context.GameConsole.Where(gameConsole => gameConsole.Brand == brand).ToListAsync();
+
+        if (consoles != null)
+        {
+            return Ok(consoles);
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
 
 
 
